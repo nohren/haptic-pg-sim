628c766c6a798005d44edb4dfcd29bb48f423ff6
Rahul Prabhu <98982995+Ruprabhu25@users.noreply.github.com>
2025-03-02 16:29:17 -0800

    implement initial moveBrake

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -112,19 +112,22 @@ void Simulation::updateRESPONSE() {
 // radius 0.0235m
 // circumference 2pi*r = 0.1476m = 2pi radians = 1 rotation
 // 1 Meter = 6.77 rotations or 42.54 radians
-void Simulation::moveBrake(float targetPosition, float targetVelocity, MountSide side, ComponentID motor) {
+void Simulation::moveBrake(float targetPosition, float targetVelocity, ComponentID motor) {
   targetAngle = targetPosition / 0.15
   if (motor == ComponentID::ZERO) {
-    targetVelocity_0 = side == MountSide::LEFT ? -targetVelocity : targetVelocity;
-    targetPosition_0 = side == MountSide::LEFT ? -targetPosition: targetPosition;
+    targetVelocity_0 = side == ComponentID::ZERO ? -targetVelocity : targetVelocity;
+    targetPosition_0 = side == ComponentID::ZERO ? -targetPosition: targetPosition;
     //convert targetPosition meters to radians - 2pi radians or 6.28 is approx equal to 144mm
-    motor.controller = MotionControlType::angle;
-    motor.velocity_limit = targetVelocity;
-    motor_0.move(targetAngle)
-    //adjust 
+    motor_0.controller = MotionControlType::angle;
+    motor_0.velocity_limit = targetVelocity / 0.15;
+    motor_0.move()
   } else {
     targetVelocity_1 = targetVelocity;
-    targetPosition_1 = side == MountSide::LEFT ? -targetPosition: targetPosition;
+    targetPosition_1 = side == ComponentID::ZERO ? targetPosition: -targetPosition;
+    //convert targetPosition meters to radians - 2pi radians or 6.28 is approx equal to 144mm
+    motor_1.controller = MotionControlType::angle;
+    motor_1.velocity_limit = targetVelocity_1 / 0.15;
+    motor_1.move()
   }
 }
 
