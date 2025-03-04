17d9c9746e3d9c3637df24e8e3472284835d27cf
y242yang <ann.ymyang@gmail.com>
2025-03-04 12:18:25 -0800

    completely working for 1 motor

--- include/simulation.h
+++ include/simulation.h
@@ -5,21 +5,30 @@
 #include "math.h"
 
 /*
-IDLE---->CALIBRATION---->RESET---->RANDOM_NOISE---->INCIDENT---->RESPONSE
-                           ^             |                           |
-                           |_____________|___________________________|
+IDLE---->CALIBRATION---->RANDOM_NOISE---->INCIDENT---->RESPONSE
+             ^                                            |
+             |____________________________________________|
 
 */
 enum class SimulationState : uint8_t {
   IDLE = 0,
   CALIBRATION,
-  RESET,
   RANDOM_NOISE,
   INCIDENT,
   RESPONSE,
   INVALID
 };
 
+inline String SimulationStateToString(SimulationState s) {
+    switch (s) {
+      case SimulationState::IDLE:   return "IDLE";
+      case SimulationState::CALIBRATION: return "CALIBRATION";
+      case SimulationState::RANDOM_NOISE:  return "RANDOM_NOISE";
+      case SimulationState::INCIDENT: return "INCIDENT";
+      case SimulationState::RESPONSE: return "RESPONSE";
+      default:    return "UNKNOWN";
+    }
+}
 //------------------------------MOTOR----------------------------
 
 enum class MotorState {
@@ -59,16 +68,29 @@ class Simulation {
         motor_1 = cur_motor_1;
     }
 
+    void initSingle(Encoder* cur_encoder_0, BLDCDriver* cur_driver_0, BLDCMotor* cur_motor_0) {
+        encoder_0 = cur_encoder_0;
+        driver_0 = cur_driver_0;
+        motor_0 = cur_motor_0;
+    }
+
+    // Update related variables after entering a new state
+    void newState();
+
     // Resting state for all components and waiting for motion
     void updateIDLE(); 
 
     // Set target position on motors 
     void updateCALIBRATION();
-    void updateRESET();
+
+    // Mimic flying
     void updateRANDOM_NOISE();
+
+    // Drop one of the motors to a specific position
     void updateINCIDENT();
+
+    // React to the other motor's movement
     void updateRESPONSE();
-    void newState();
 
   private:
     Encoder* getEncoder(ComponentID component_id) {
@@ -88,7 +110,7 @@ class Simulation {
 
     bool atLocationForMotor(ComponentID motor_id, float position);
 
-    void moveMotor(ComponentID motor_id, float targetPosition, float targetVelocity);
+    void moveMotor(ComponentID motor_id, float targetPosition, float targetVelocity, float targetVoltage);
 
     ComponentID selectRandomMotor();
 
@@ -99,20 +121,33 @@ class Simulation {
 
     // ----------- parameters ------------ 
     float calibrated_position = 0.0;
-    ComponentID incident_motor = ComponentID::ZERO;
-    float incident_position = 20.0;
+    float incident_position = 10.0;
 
-    unsigned long reset_state_wait = 5000000UL;
+    unsigned long calibration_wait = 5000000UL;
     unsigned long response_time_wait = 3000000UL;
     unsigned long random_noise_wait = -1UL;
+    unsigned long random_noise_interval = 2000000UL;
 
+    // more than half a rotation is considered an intentional movement
     float movement_threshold = 0.5 * _PI; // 1/4 rotation
+
+    // within 1/20 rotation is considered stable position
     float stablized_threshold = 0.1 * _PI; // 1/20 rotation
+
+    // move back and forth within 1/10 rotation
     float random_noise_threshold = 0.2 * _PI; // 1/10 rotation
     int last_random_noise_direction = 1;
-    float reset_velocity = 0.15;
-    float random_noise_velocity = 0.1;
-    float incident_velocity = 0.8;
+
+    float calibration_voltage = 11.0;
+    float calibration_velocity = 4.0;
+    float random_noise_voltage = 5.0;
+    float random_noise_velocity = 10.0;
+    float incident_voltage = 10.0;
+    float incident_velocity = 10.0;
+    float response_voltage = 5.0;
+    float response_velocity = 10.0;
+
+    ComponentID incident_motor = ComponentID::ZERO;
 };
 
 #endif
